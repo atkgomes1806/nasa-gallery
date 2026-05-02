42acc48729cea493c04aeadb4dc265ba8c9a3257
Arthur Gomes <atkgomes@gmail.com>
2026-05-01 21:25:19 -0300

    Splash: manter GIF animado (remover congelamento em primeiro frame)

--- lib/features/splash/splash.dart
+++ lib/features/splash/splash.dart
@@ -1,7 +1,4 @@
-import 'dart:ui' as ui;
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:nasa_app/features/home_page/home_page.dart';
 
 class Splash extends StatefulWidget {
@@ -12,36 +9,16 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-  ui.Image? _firstFrame;
-
   @override
   void initState() {
     super.initState();
-    _loadFirstFrame();
     Future.delayed(const Duration(seconds: 2), () {
       if (mounted) {
-        Navigator.of(
-          context,
-        ).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
       }
     });
-  }
-
-  Future<void> _loadFirstFrame() async {
-    try {
-      final ByteData data = await rootBundle.load(
-        'assets/images/nasa_logo.gif',
-      );
-      final Uint8List bytes = data.buffer.asUint8List();
-      final ui.Codec codec = await ui.instantiateImageCodec(bytes);
-      final ui.FrameInfo frameInfo = await codec.getNextFrame();
-      setState(() {
-        _firstFrame = frameInfo.image;
-      });
-    } catch (_) {
-      // se falhar, não quebra a tela; imagem ficará nula até Image.asset carregar
-      setState(() => _firstFrame = null);
-    }
   }
 
   @override
@@ -65,13 +42,16 @@ class _SplashState extends State<Splash> {
           child: SizedBox(
             width: 320,
             height: 320,
-            child: _firstFrame != null
-                ? RawImage(image: _firstFrame, fit: BoxFit.contain)
-                : Image.asset(
-                    'assets/images/nasa_logo.gif',
-                    fit: BoxFit.contain,
-                  ),
+            child: Image.asset(
+              'assets/images/nasa_logo.gif',
+              fit: BoxFit.contain,
+            ),
           ),
+        ),
+      ),
+    );
+  }
+}
         ),
       ),
     );
